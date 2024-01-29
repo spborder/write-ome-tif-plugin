bdd0a8abba013443f6dd56cef185f96a6769f1c4
spborder <spborder@buffalo.edu>
2024-01-29 15:45:41 -0500

    Getting image file id from input

--- write_ome_tiff/cli/WriteOMETIFF/WriteOMETIFF.py
+++ write_ome_tiff/cli/WriteOMETIFF/WriteOMETIFF.py
@@ -16,12 +16,16 @@ from cli.write_ome_tif import OMETIFFMaker
 
 def main(args):
 
-    image_id = args.input_image.split(os.sep)[-1]
+    image_file_id = args.input_image
+    # This is the file id, not the item which has annotations. So have to get the associated item id
+
     girderApiUrl = args.girderApiUrl
     girderToken = args.girderToken
 
     gc = girder_client.GirderClient(apiUrl=girderApiUrl)
     gc.setToken(girderToken)
+
+    image_id = gc.get(f'/resource/{image_file_id}?token={girderToken}')['itemId']
 
     annotations = gc.get(f'/annotation/item/{image_id}')
 
